df469c313cc2987965bd42a866aa9218833596b6
Phantas Weng <bensonuong@gmail.com>
2020-05-14 11:03:22 +0000

    Split header to layout folder

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,7 @@
         <sidebarNavigation></sidebarNavigation>
       </div>
       <div class="mask__main">
-        <div class="mask__main__header">
-          <div class="mask__main__header__application">
-            <img src="~@/assets/logo.png" alt="OwlNews Logo" class="mask__main__header__application__logo">
-            <h2 class="mask__main__header__application__title">奧丁丁 OwlTing</h2>
-          </div>
-          <div class="mask__main__header__control">
-            <div class="mask__main__header__user">
-              <img src="https://lh3.google.com/u/0/ogw/ADGmqu_U_avM1xZlSsLYty6vHruZQFOYUzOQdQ_ojOxJ=s83-c-mo">
-            </div>
-          </div>
-        </div>
+        <mainHeader></mainHeader>
         <div class="mask__main__view">
           <router-view></router-view>
         </div>
@@ -30,11 +20,13 @@
 <script>
 import sidebarLogo from '@/layout/sidebar/logo'
 import sidebarNavigation from '@/layout/sidebar/navigation'
+import mainHeader from '@/layout/main/header'
 
 export default {
   components: {
     sidebarLogo,
-    sidebarNavigation
+    sidebarNavigation,
+    mainHeader
   },
   data () {
     return {
@@ -65,21 +57,6 @@ export default {
     min-width: 200px
   &__main
     @apply flex-auto text-green
-    &__header
-      @apply flex items-center p-8 justify-between
-      @apply border-b border-grey-lightest
-      min-height: 68px
-      &__application
-        @apply inline-flex flex-wrap items-center
-        &__logo
-          @apply inline-block m-8
-          width: 36px
-        &__title
-          @apply text-grey-darkest font-bold
-      &__control
-      &__user
-        width: 40px
-        @apply overflow-hidden rounded-full
     &__view
       @apply p-16
 </style>
---
+++ src/layout/main/header.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="mask__main__header">
+  <div class="mask__main__header__application">
+    <img src="~@/assets/logo.png" alt="OwlNews Logo" class="mask__main__header__application__logo">
+    <h2 class="mask__main__header__application__title">奧丁丁 OwlTing</h2>
+  </div>
+  <div class="mask__main__header__control">
+    <div class="mask__main__header__user">
+      <img src="https://lh3.google.com/u/0/ogw/ADGmqu_U_avM1xZlSsLYty6vHruZQFOYUzOQdQ_ojOxJ=s83-c-mo">
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="sass" scoped>
+.mask__main__header
+  @apply flex items-center p-8 justify-between
+  @apply border-b border-grey-lightest
+  min-height: 68px
+  &__application
+    @apply inline-flex flex-wrap items-center
+    &__logo
+      @apply inline-block m-8
+      width: 36px
+    &__title
+      @apply text-grey-darkest font-bold
+  &__control
+  &__user
+    width: 40px
+    @apply overflow-hidden rounded-full
+</style>
