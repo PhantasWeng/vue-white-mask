167ff461915b05be5fc4bf48f314d01646712e01
Phantas Weng <bensonuong@gmail.com>
2020-05-27 19:47:02 +0800

    Move navigation structor to mask.config

--- src/layout/sidebar/navigation.vue
+++ src/layout/sidebar/navigation.vue
@@ -1,6 +1,6 @@
 <template>
 <nav class="mask__navigation">
-  <template v-for="(group, index) in data">
+  <template v-for="(group, index) in router">
     <div :key="group.name">
       <template v-for="block in group.children">
         <div class="mask__navigation__block" :key="`${group.name}-${block.name}`">
@@ -12,7 +12,7 @@
           </template>
         </div>
       </template>
-      <hr v-if="index + 1 < data.length" class="mask__navigation__hr">
+      <hr v-if="index + 1 < router.length" class="mask__navigation__hr">
     </div>
   </template>
   <router-link :to="{name: 'Login'}">login</router-link>
@@ -21,173 +21,14 @@
 </template>
 
 <script>
+import { router } from '@/mask.config.js'
 export default {
   data () {
     return {
-      data: [
-        {
-          name: 'website',
-          type: 'group',
-          children: [
-            {
-              name: '儀表版',
-              key: 'dashboard',
-              type: 'page'
-            },
-            {
-              name: '前台',
-              type: 'block',
-              children: [
-                {
-                  key: 'homepage-edit',
-                  name: '首頁編輯',
-                  type: 'page'
-                },
-                {
-                  key: 'policy-edit',
-                  name: '政策/條款',
-                  type: 'page'
-                },
-                {
-                  key: 'phantas',
-                  name: 'Phantas',
-                  type: 'link'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          name: 'hotels',
-          type: 'group',
-          children: [
-            {
-              name: '旅宿',
-              type: 'block',
-              children: [
-                {
-                  key: 'hotel-list',
-                  name: '旅宿列表',
-                  type: 'page'
-                },
-                {
-                  key: 'hotel-apply',
-                  name: '申請列表',
-                  type: 'page'
-                },
-                {
-                  key: 'hotel-outer',
-                  name: '外部房源',
-                  type: 'link'
-                }
-              ]
-            },
-            {
-              name: '標籤',
-              type: 'block',
-              children: [
-                {
-                  key: 'projects-list',
-                  name: '行銷活動',
-                  type: 'page'
-                },
-                {
-                  key: 'invitations-list',
-                  name: '邀請回饋',
-                  type: 'page'
-                },
-                {
-                  key: 'coupons',
-                  name: '優惠券',
-                  type: 'link'
-                },
-                {
-                  key: 'events-list',
-                  name: '活動列表',
-                  type: 'link'
-                }
-              ]
-            },
-            {
-              name: '財務',
-              type: 'block',
-              children: [
-                {
-                  key: 'finance-refund',
-                  name: '銷退',
-                  type: 'page'
-                },
-                {
-                  key: 'finance-accounts',
-                  name: '帳務',
-                  type: 'page'
-                },
-                {
-                  key: 'finance-hotels',
-                  name: '飯店帳務',
-                  type: 'link'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          name: 'user',
-          type: 'group',
-          children: [
-            {
-              name: '會員',
-              type: 'block',
-              children: [
-                {
-                  key: 'user-list',
-                  name: '會員列表',
-                  type: 'page'
-                },
-                {
-                  key: 'user-subscription',
-                  name: '訂閱',
-                  type: 'page'
-                }
-              ]
-            },
-            {
-              name: '評論',
-              type: 'block',
-              children: [
-                {
-                  key: 'comments-list',
-                  name: '評論列表',
-                  type: 'page'
-                },
-                {
-                  key: 'comments-nights',
-                  name: '入住夜間',
-                  type: 'page'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          name: 'system',
-          type: 'group',
-          children: [
-            {
-              name: '系統',
-              type: 'block',
-              children: [
-                {
-                  key: 'system-auth',
-                  name: '權限',
-                  type: 'page'
-                }
-              ]
-            }
-          ]
-        }
-      ]
+      router: router
     }
+  },
+  mounted () {
   }
 }
 </script>
--- src/mask.config.js
+++ src/mask.config.js
@@ -1,12 +1,170 @@
-module.exports = {
+const mask = {
   title: 'whiteMask',
   router: [
     {
-      hotels: {
-        path: '/hotels',
-        name: 'hotels',
-        component: () => import('@/views/Hotels')
-      }
+      name: 'website',
+      type: 'group',
+      children: [
+        {
+          name: '儀表版',
+          key: 'dashboard',
+          type: 'page'
+        },
+        {
+          name: '前台',
+          type: 'block',
+          children: [
+            {
+              key: 'homepage-edit',
+              name: '首頁編輯',
+              type: 'page'
+            },
+            {
+              key: 'policy-edit',
+              name: '政策/條款',
+              type: 'page'
+            },
+            {
+              key: 'phantas',
+              name: 'Phantas',
+              type: 'link'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'hotels',
+      type: 'group',
+      children: [
+        {
+          name: '旅宿',
+          type: 'block',
+          children: [
+            {
+              key: 'hotel-list',
+              name: '旅宿列表',
+              type: 'page'
+            },
+            {
+              key: 'hotel-apply',
+              name: '申請列表',
+              type: 'page'
+            },
+            {
+              key: 'hotel-outer',
+              name: '外部房源',
+              type: 'link'
+            }
+          ]
+        },
+        {
+          name: '標籤',
+          type: 'block',
+          children: [
+            {
+              key: 'projects-list',
+              name: '行銷活動',
+              type: 'page'
+            },
+            {
+              key: 'invitations-list',
+              name: '邀請回饋',
+              type: 'page'
+            },
+            {
+              key: 'coupons',
+              name: '優惠券',
+              type: 'link'
+            },
+            {
+              key: 'events-list',
+              name: '活動列表',
+              type: 'link'
+            }
+          ]
+        },
+        {
+          name: '財務',
+          type: 'block',
+          children: [
+            {
+              key: 'finance-refund',
+              name: '銷退',
+              type: 'page'
+            },
+            {
+              key: 'finance-accounts',
+              name: '帳務',
+              type: 'page'
+            },
+            {
+              key: 'finance-hotels',
+              name: '飯店帳務',
+              type: 'link'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'user',
+      type: 'group',
+      children: [
+        {
+          name: '會員',
+          type: 'block',
+          children: [
+            {
+              key: 'user-list',
+              name: '會員列表',
+              type: 'page'
+            },
+            {
+              key: 'user-subscription',
+              name: '訂閱',
+              type: 'page'
+            }
+          ]
+        },
+        {
+          name: '評論',
+          type: 'block',
+          children: [
+            {
+              key: 'comments-list',
+              name: '評論列表',
+              type: 'page'
+            },
+            {
+              key: 'comments-nights',
+              name: '入住夜間',
+              type: 'page'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'system',
+      type: 'group',
+      children: [
+        {
+          name: '系統',
+          type: 'block',
+          children: [
+            {
+              key: 'system-auth',
+              name: '權限',
+              type: 'page'
+            }
+          ]
+        }
+      ]
     }
   ]
 }
+const router = mask.router
+export { router }
+
+export default mask
