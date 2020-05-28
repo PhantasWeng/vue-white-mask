8a6c9035b0506fabe2c04f166d0aebd3366e6977
Phantas Weng <bensonuong@gmail.com>
2020-05-28 20:02:21 +0800

    Update dashboard layout

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -5,26 +5,19 @@
       <h1>Dashboard</h1>
       <a-tag color="orange" class="ml-8">v0.1</a-tag>
     </div>
-    <a-row type="flex">
-      <a-col :xs="24" :lg="8" :xl="6">
-        <a-card>
-          <img slot="cover" alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    <div class="md:flex">
+      <a-card class="border border-1 border-grey-lightest" :bordered="false">
+        <img slot="cover" alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+        <a-card-meta title="whiteMask" description="A Vue admin framework" :bordered="false">
+          <a-avatar
+            slot="avatar"
+            src="https://lh3.google.com/u/0/ogw/ADGmqu_U_avM1xZlSsLYty6vHruZQFOYUzOQdQ_ojOxJ=s83-c-mo"
           />
-          <template slot="actions" class="ant-card-actions">
-            <a-icon key="setting" type="setting" />
-            <a-icon key="edit" type="edit" />
-            <a-icon key="ellipsis" type="ellipsis" />
-          </template>
-          <a-card-meta title="whiteMask" description="A Vue admin framework">
-            <a-avatar
-              slot="avatar"
-              src="https://lh3.google.com/u/0/ogw/ADGmqu_U_avM1xZlSsLYty6vHruZQFOYUzOQdQ_ojOxJ=s83-c-mo"
-            />
-          </a-card-meta>
-        </a-card>
-      </a-col>
-      <a-col :xs="24" :lg="16" :xl="18">
-        <a-card class="lg:mx-8 mt-8 lg:mt-0 mb-8">
+        </a-card-meta>
+      </a-card>
+      <div class="flex-auto">
+        <a-card class="lg:mx-8 mt-8 lg:mt-0 mb-8 border border-1 border-grey-lightest" :bordered="false">
           <template>
             <div>
               <a-progress :percent="30" />
@@ -35,7 +28,7 @@
             </div>
           </template>
         </a-card>
-        <a-card class="lg:mx-8">
+        <a-card class="lg:mx-8 border border-1 border-grey-lightest" :bordered="false">
           <a-statistic title="Road Map" :value="10" style="margin-right: 50px">
             <template #suffix>
               <span>/100</span>
@@ -47,9 +40,11 @@
             </template>
           </a-statistic>
         </a-card>
-      </a-col>
-    </a-row>
-    <a-row></a-row>
+      </div>
+    </div>
+    <div class="border border-1 border-grey-lightest p-16 my-8">
+      <a-skeleton />
+    </div>
   </div>
 </template>
 
